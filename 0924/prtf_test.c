3dbeb6f204f4ff48c9963d1256fd053031bff65e
LeeJun <ljun1118@ks.ac.kr>
2019-09-24 18:26:30 +0900

    20190924

---
+++ 0924/prtf_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("Hello C world\n");
+
+	printf ("%d\n" ,printf("Hello C world\n"));
+	return 0;
+}
---
+++ 0924/test1.c
@@ -0,0 +1,14 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+
+int main(void)
+{
+
+	struct stat buf;
+	stat("test1.txt",&buf);
+	printf("st_mode = %o\n",buf.st_mode);
+
+	return 0;
+}
