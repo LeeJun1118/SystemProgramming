055fe91cf9761220f3ec75678eb6b41e761a3668
LeeJun <ljun1118@ks.ac.kr>
2019-10-29 11:49:18 +0900

    1029수업

---
+++ 1029/buffer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+	int i,n,m,j;
+	char c;
+	printf("반복할 수 입력: ");
+	scanf("%d",&n);
+	for(i = 1; i <= n; i++)
+	{
+		printf("반복할 문자와 반복할 수 입력: ");
+		getchar();
+		//__wur(stdin);
+		//__fpurge(stdin);
+		scanf("%c %d",&c,&m);
+		for(j = 1;j <=m; j++)
+		{
+			printf("%c",c);
+		}
+		printf("\n");
+	}
+
+}
